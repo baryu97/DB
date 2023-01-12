276fd6f89ecdea679da8a841e59e3b22a063817d
baryu97 <baryu97@hanyang.ac.kr>
2023-01-13 01:44:25 +0900

    change cmakelists

---
+++ build/db/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yesung/Desktop/DB/db
+# Build directory: /home/yesung/Desktop/DB/build/db
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/test/db_test[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/yesung/Desktop/DB/build/test/db_test[1]_tests.cmake")
+  include("/home/yesung/Desktop/DB/build/test/db_test[1]_tests.cmake")
+else()
+  add_test(db_test_NOT_BUILT db_test_NOT_BUILT)
+endif()
